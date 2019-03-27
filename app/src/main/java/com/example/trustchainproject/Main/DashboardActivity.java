01690c46748747fdfbdc8239581320c0ff0447ae
Thomas <thomas@shoppingminds.com>
2019-03-27 13:47:55 +0100

    connector shit

--- app/src/main/java/com/example/trustchainproject/Main/DashboardActivity.java
+++ app/src/main/java/com/example/trustchainproject/Main/DashboardActivity.java
@@ -5,10 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.trustchainproject.R;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection
+                    ("jdbc:mysql://10.0.2.2:3306/stock", "root", "root");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
     }
