d01e74c6118609fd4c96912a1137df3daecc59ad
Bart van Straaten <bartvanstraaten98@gmail.com>
2019-04-02 13:10:16 +0200

    Reverse merge

--- app/src/main/java/com/studielink/trustchain/qr/exception/QRWalletValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.studielink.trustchain.qr.exception;
-
-public class QRWalletValidationException extends QRWalletImportException {
-    public QRWalletValidationException(Exception cause) {
-        super(cause);
-    }
-}
---
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/Storage/Account.java
@@ -0,0 +1,48 @@
+package nl.tudelft.cs4160.trustchain_android.Storage;
+
+public class Account {
+
+    private String username;
+    private String name;
+    private String surname;
+    private Integer age;
+
+    public Account(String username, String name, String surname, Integer age) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
