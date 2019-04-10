f8542eecfe3e8c585c049f59b302358e4468e8c5
Bart van Straaten <bartvanstraaten98@gmail.com>
2019-04-10 12:18:32 +0200

    Implemented some functions

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/Main/InstitutionHomeActivity.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/Main/InstitutionHomeActivity.java
@@ -1,6 +1,12 @@
 package nl.tudelft.cs4160.trustchain_android.Main;
 
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -16,11 +22,92 @@ import nl.tudelft.cs4160.trustchain_android.AppToApp.connection.messages.Message
 import nl.tudelft.cs4160.trustchain_android.AppToApp.connection.messages.MessageException;
 import nl.tudelft.cs4160.trustchain_android.AppToApp.connection.messages.Puncture;
 import nl.tudelft.cs4160.trustchain_android.AppToApp.connection.messages.PunctureRequest;
+import nl.tudelft.cs4160.trustchain_android.Block.TrustChainBlockHelper;
+import nl.tudelft.cs4160.trustchain_android.Crypto.DualSecret;
+import nl.tudelft.cs4160.trustchain_android.Crypto.Key;
+import nl.tudelft.cs4160.trustchain_android.Funds.FundsActivity;
+import nl.tudelft.cs4160.trustchain_android.Message.MessageProto;
 import nl.tudelft.cs4160.trustchain_android.Network.NetworkCommunicationListener;
+import nl.tudelft.cs4160.trustchain_android.QR.ExportWalletQRActivity;
+import nl.tudelft.cs4160.trustchain_android.QR.ScanQRActivity;
+import nl.tudelft.cs4160.trustchain_android.R;
+import nl.tudelft.cs4160.trustchain_android.Storage.TrustChainDBHelper;
+
+import static nl.tudelft.cs4160.trustchain_android.Block.TrustChainBlockHelper.GENESIS_SEQ;
 
 public class InstitutionHomeActivity extends AppCompatActivity implements NetworkCommunicationListener, PeerListener {
+
+    private TrustChainDBHelper dbHelper;
+
     @Override
-    on
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initKey();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chain_menu:
+                Intent chainExplorerActivity = new Intent(this, ChainExplorerActivity.class);
+                startActivity(chainExplorerActivity);
+                return true;
+            case R.id.connection_explanation_menu:
+                Intent ConnectionExplanationActivity = new Intent(this, ConnectionExplanationActivity.class);
+                startActivity(ConnectionExplanationActivity);
+                return true;
+            case R.id.import_tokens:
+                startActivity(new Intent(this, ScanQRActivity.class));
+                return true;
+            case R.id.export_tokens:
+                startActivity(new Intent(this, ExportWalletQRActivity.class));
+                return true;
+            case R.id.funds:
+                startActivity(new Intent(this, FundsActivity.class));
+                return true;
+            case R.id.find_peer:
+                Intent bootstrapActivity = new Intent(this, BootstrapActivity.class);
+                startActivityForResult(bootstrapActivity, 1);
+            case R.id.clear_data:
+                ((ActivityManager) getApplicationContext().getSystemService(ACTIVITY_SERVICE))
+                        .clearApplicationUserData();
+            default:
+                return true;
+        }
+    }
+
+    /**
+     * If the app is launched for the first time
+     * a new keyPair is created and save locally in the storage.
+     * A genesis block is also created automatically.
+     */
+    private void initKey() {
+        DualSecret kp = Key.loadKeys(getApplicationContext());
+        if (kp == null) {
+            kp = Key.createAndSaveKeys(getApplicationContext());
+        }
+        if (isStartedFirstTime(dbHelper, kp)) {
+            MessageProto.TrustChainBlock block = TrustChainBlockHelper.createGenesisBlock(kp);
+            dbHelper.insertInDB(block);
+        }
+    }
+
+    /**
+     * Checks if this is the first time the app is started and returns a boolean value indicating
+     * this state.
+     *
+     * @return state - false if the app has been initialized before, true if first time app started
+     */
+    public boolean isStartedFirstTime(TrustChainDBHelper dbHelper, DualSecret kp) {
+        // check if a genesis block is present in database
+        MessageProto.TrustChainBlock genesisBlock = dbHelper.getBlock(kp.getPublicKeyPair().toBytes(), GENESIS_SEQ);
+        return (genesisBlock == null);
+    }
 
     @Override
     public void updateIncomingPeers() {
